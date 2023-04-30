38cb6f592effc10038974164594450d407968d01
Anesidora II <vickbelo17@gmail.com>
2023-04-29 22:37:23 -0300

    mapeando swagger

--- src/controller/cdi.controller.ts
+++ src/controller/cdi.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get } from "@nestjs/common";
-import { ApiResponse, ApiTags } from "@nestjs/swagger";
+import {  ApiResponse, ApiTags } from "@nestjs/swagger";
 import { AppService } from "src/app.service";
 
 @Controller('/v1')
@@ -9,6 +9,7 @@ export class CDIController {
     @ApiTags('cdi')
     @ApiResponse({ status: 200, description: 'The request has been successfully.'})
     @ApiResponse({ status: 400, description: 'Bad Request.'})
+    
     @Get('/cdi')
     async cdiApp(){
       return this.service.getCDI();
