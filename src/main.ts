274ac8b927f20b080ff86971523de7162dccd55d
Victória <86816104+Victoria-Belo@users.noreply.github.com>
2023-10-22 15:05:05 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,16 +8,9 @@ declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(
     AppModule,
-    new ExpressAdapter() 
+    new ExpressAdapter(),
   );
-
-  // Configure as opções de CORS
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
-    credentials: true,
-  });
-
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('Certificado de Depósito Interbancário Mensal')
     .setDescription('Taxa de juros entre bancos')
@@ -25,10 +18,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/api', app, document);
-
-
   await app.listen(3000);
-
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
