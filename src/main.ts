71c42013961258313db458d58d77e597a437180c
Victória <86816104+Victoria-Belo@users.noreply.github.com>
2023-10-22 14:06:19 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
     .addTag('cdi')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('cdi-app/v1/cdi', app, document);
+  SwaggerModule.setup('cdi-app', app, document);
 
 
   await app.listen(3000);
